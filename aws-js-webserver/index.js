cb328a9e839edbe6e85ce14b675cc27cc8163e4e
Tomas <40318863+tomasmik@users.noreply.github.com>
2023-02-13 17:21:35 +0200

    Update index.js

--- aws-js-webserver/index.js
+++ aws-js-webserver/index.js
@@ -22,19 +22,10 @@ let group = new aws.ec2.SecurityGroup("web-secgrp", {
     ],
 }, { aliases: [{ name: "new-web-secgrp" }] });
 
-// (optional) create a simple web server using the startup script for the instance
-let userData =
-`#!/bin/bash
-echo "Hello, World!" > index.html
-nohup python -m SimpleHTTPServer 80 &`;
-
-let server = new aws.ec2.Instance("web-server-www", {
-    tags: { "Name": "web-server-www" },
-    instanceType: size,
-    vpcSecurityGroupIds: [ group.id ], // reference the group object above
-    ami: ami,
-    userData: userData              // start a simple web server
+// create a new security group for port 80
+let group2 = new aws.ec2.SecurityGroup("web-secgrp", {
+    ingress: [
+        { protocol: "tcp", fromPort: 22, toPort: 22, cidrBlocks: ["0.0.0.0/0"] },
+        { protocol: "tcp", fromPort: 80, toPort: 80, cidrBlocks: ["0.0.0.0/0"] },
+    ],
 }, { parent: group });
-
-exports.publicIp = server.publicIp;
-exports.publicHostName = server.publicDns;
